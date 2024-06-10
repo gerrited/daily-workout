694e3207964c0020ad7718c260b82542b4107bb0
Gerrit Edzards <gerrit.edzards@gmail.com>
2024-06-10 08:55:25 +0200

    PoC of ai generated workout with Open AI

--- src/workout-generator/app.js
+++ src/workout-generator/app.js
@@ -1,5 +1,14 @@
+const OpenAI = require("openai");
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config();
+
+const configuration = {
+  apiKey: process.env.OPENAI_API_KEY,
+};
+
+const openai = new OpenAI(configuration);
+const prompt = 'Create a random workout with exercises, reps, and sets for a full-body workout.';
 
 function generateWorkout() {
   return {
@@ -12,6 +21,26 @@ function generateWorkout() {
     ]
   };
 }
+
+async function generateAIWorkout() {
+  try {
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [{"role": "user", "content": prompt}],
+      max_tokens: 150,
+      n: 1,
+      stop: null,
+      temperature: 0.7,
+    });
+
+    const workout = response.choices[0]
+    console.log('Generated Workout:\n', workout);
+  } catch (error) {
+    console.error('Error generating workout:', error.response ? error.response.data : error.message);
+  }
+}
+
+generateAIWorkout();
 
 const dirPath = path.join(__dirname, 'data');
 const filePath = path.join(dirPath, 'workout.json');
